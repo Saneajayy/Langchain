681eed61f76462ee89f4c96cd5a8999d50e8ba51
saneajayy <ajaykumarrj302@gmail.com>
2026-03-07 19:33:22 +0530

    String Output parser.

---
+++ Langchain_outputs/JSON_op.py
@@ -0,0 +1,18 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import JsonOutputParser
+
+
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="google/gemma-2-2b-it",
+    task="text-generation",
+    max_new_tokens=512,
+    temperature=0.5
+)
+
+model = ChatHuggingFace(llm=llm)
+
